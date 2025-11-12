cfb1667fefd12b4e5c715f7800ab302f26739cdb
md-mahmudu1-hasan <mdmahmudulhasan0008@gmail.com>
2025-11-12 17:53:44 +0600

    .env file create

--- src/Utilities/firebase.init.js
+++ src/Utilities/firebase.init.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyCkT-irKri1e3wp9uyY51IdS3q9aw-3z_o",
-  authDomain: "authentication-habit-tracker.firebaseapp.com",
-  projectId: "authentication-habit-tracker",
-  storageBucket: "authentication-habit-tracker.firebasestorage.app",
-  messagingSenderId: "163311706195",
-  appId: "1:163311706195:web:a931f60349fe60387b26a6",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 const app = initializeApp(firebaseConfig);
